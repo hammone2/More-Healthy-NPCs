0c7272e43c80025d1f19df49056b194c4d166277
hammone2 <hammone2@my.erau.edu>
2025-11-07 11:36:29 -0700

    Initialize Project

---
+++ More Healthy NPCs/Assets/Scripts/NPC.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float turnSpeed = 90f;
+    [SerializeField] private int startingHp = 100;
+    [SerializeField] private UnityEngine.UI.Slider hpBarSlider = null;
+
+    private void Start()
+    {
+        GetComponent<IHealth>().OnDied += Die;
+    }
+
+    internal void TakeDamage(int amount)
+    {
+        GetComponent<IHealth>().TakeDamage(amount);
+    }
+
+    private void Update()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        transform.Rotate(0f, turnSpeed * Time.deltaTime, 0f);
+        hpBarSlider.transform.LookAt(Camera.main.transform);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(startingHp / 10);
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
